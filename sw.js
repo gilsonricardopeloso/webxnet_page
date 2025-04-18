a8e3f9aff20d9dc1d150083508cd37813b97ef83
Gilson Ricardo Peloso <gilson.peloso@webxnet.com.br>
2025-04-18 12:58:26 -0300

    PWA, sw.js, index.html - correções

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-var staticCacheName = "wxn-2020-05-22d"
+var staticCacheName = "wxn-2025-04-18d"
 var filesToCache = [
   "/",
   "/index.html",
@@ -8,13 +8,36 @@ var filesToCache = [
   "/selo-google-site-seguro.png",
 ]
 // Cache on install
-this.addEventListener("install", (event) => {
-  this.skipWaiting()
+self.addEventListener("install", (event) => {
+  self.skipWaiting()
   console.log("[Service Worker] Install")
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log("[Service Worker] Caching all files")
       return cache.addAll(filesToCache)
+    })
+  )
+})
+
+// Clean up old caches on activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== staticCacheName)
+          .map((name) => caches.delete(name))
+      )
+    })
+  )
+  self.clients.claim()
+})
+
+// Serve from cache, fallback to network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
     })
   )
 })
